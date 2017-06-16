d4f346adc2fe10ef514cbf5776f31ff752d316e1
Rob Moore <giftiger.wunsch@xantoria.com>
2017-06-16 21:40:46 +0100

    Rm ammonite script
    And fix main method

--- src/main/scala/com/xantoria/screenshooter/Main.scala
+++ src/main/scala/com/xantoria/screenshooter/Main.scala
@@ -2,8 +2,8 @@ package com.xantoria.screenshooter
 
 import java.awt.{Point, Rectangle}
 
-class Main {
-  def main(args: List[String]): Unit = {
+object Main {
+  def main(args: Array[String]): Unit = {
     val shot = Utils.captureScreen()
     val screenshotter = new Screenshotter(Utils.bufferImage(shot))
     screenshotter.main(Array.empty[String])
